5a6ac079894b24af0a6f7f1cd909d40305b10506
jm1034369 <jm1034369@wcupa.edu>
2025-04-07 17:57:39 -0400

    Commit 2

--- EmailFeatures.java
+++ EmailFeatures.java
@@ -1,2 +1,18 @@
 public class EmailFeatures {
+    public int wordCount;
+    public int specialCharCount;
+    public int uppercaseWordCount;
+    public int linkCount;
+    public double avgWordLength;
+
+    public EmailFeatures(int wordCount, int specialCharCount, int uppercaseWordCount, int linkCount, double avgWordLength) {
+        this.wordCount = wordCount;
+        this.specialCharCount = specialCharCount;
+        this.uppercaseWordCount = uppercaseWordCount;
+        this.linkCount = linkCount;
+        this.avgWordLength = avgWordLength;
+    }
+    public double[] toVector() {
+        return new double[] {wordCount, specialCharCount, uppercaseWordCount, linkCount, avgWordLength};
+    }
 }

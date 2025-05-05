9c0425c516661d895ef00706248f91652b5f97e0
davidssierra19 <DS1042782@wcupa.edu>
2025-05-04 22:12:24 -0400

    Final Update for Email-Spam-Filter

--- Email.java
+++ Email.java
@@ -1,30 +1,39 @@
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Email {
 
     private int emailID;
+    private int type;
     private String[] words;
     private EmailFeatures features;
 
-    public Email(int index, String emailText) {
+    // Constructor takes in the email number, its type (Spam or Ham), and the String of text for the email that is converted to an Array of words
+    public Email(int index, int type, String emailText) {
         emailID = index;
+        this.type = type;
+        // Splits the email into a String[] of words by using " +", which splits with one or more spaces
         String[] temp = emailText.split(" +");
+        // Sometimes, the words Array can contain a blank element, so this piece of code (until line 22) removes all of those occurrences
         ArrayList<String> tempWords = new ArrayList<>();
         for(int i = 0; i < temp.length; i++) {
             tempWords.add(temp[i]);
         }
         tempWords.removeAll(Collections.singletonList(""));
         words = tempWords.toArray(new String[0]);
-
-        features = new EmailFeatures(emailID, words);
+        features = new EmailFeatures(words);
     }
 
+    // Getters for email fields
+    public int getEmailID() {
+        return emailID;
+    }
+    public int getType() {
+        return type;
+    }
     public String[] getWords() {
         return words;
     }
-
     public EmailFeatures getFeatures() {
         return features;
     }
--- EmailFeatures.java
+++ EmailFeatures.java
@@ -1,39 +1,28 @@
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
+// This object is responsible for extracting the features of a single email
 public class EmailFeatures {
 
-    private int emailID;
     private String[] words;
 
-    private int wordCount;
     private int urlCount;
     private int specialCharacterWordCount;
     private String[][] nGramFrequency;
-    private String[][] wordFrequency;
-    private int[][] wordSizeDistribution;
-    private int minWordSize;
-    private int maxWordSize;
-    private double meanWordSize;
-    private double medianWordSize;
-    private int modeWordSize;
+    private HashMap<String, Integer> wordFrequency;
 
-    public EmailFeatures(int id, String[] words) {
-        emailID = id;
+    // Constructor for EmailFeatures; initializes each value
+    public EmailFeatures(String[] words) {
+
         this.words = words;
 
-        wordCount = words.length;
         urlCount = extractUrlCount();
-        wordFrequency = extractWordFrequency(DatasetProcessor.uniqueWords);
+        wordFrequency = extractWordFrequency();
         nGramFrequency = extractNGramFrequency(DatasetProcessor.spamNGrams);
         specialCharacterWordCount = extractSpecialCharacterWordCount();
-        wordSizeDistribution = extractWordSizeDistribution(DatasetProcessor.largestWordSize);
-        meanWordSize = extractMeanWordSize();
-        medianWordSize = extractMedianWordSize();
-        modeWordSize = extractModeWordSize();
+
     }
 
+    // Extracts the url count
     public int extractUrlCount() {
         int count = 0;
         for(int i = 0; i < words.length; i++) {
@@ -44,21 +33,16 @@ public class EmailFeatures {
         return count;
     }
 
-    public String [][] extractWordFrequency(String[] uniqueWords) {
-        Map<String, Integer> freqMap = new HashMap<>();
+    // Extracts the word frequency in the format of a HashMap
+    public HashMap<String, Integer> extractWordFrequency() {
+        HashMap<String, Integer> wordFreq = new HashMap<>();
         for(String word : words) {
-            freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
+            wordFreq.put(word, (wordFreq.getOrDefault(word, 0) + 1));
         }
-        String [][] wordFrequency = new String[uniqueWords.length] [2];
-        for(int i = 0; i < uniqueWords.length; i++) {
-            String key = uniqueWords[i];
-            int count = freqMap.getOrDefault(key, 0);
-            wordFrequency [i][0] = key;
-            wordFrequency [i][1] = Integer.toString(count);
-        }
-        return wordFrequency;
+        return wordFreq;
     }
 
+    // Extracts the spam n-gram frequency in the format of a 2D String array
     public String[][] extractNGramFrequency(String[] nGrams) {
         String[][] nGramFrequency = new String[nGrams.length][2];
         for(int i = 0; i < nGrams.length; i++) {
@@ -90,6 +74,7 @@ public class EmailFeatures {
         return nGramFrequency;
     }
 
+    // Extracts the number of words that contain a special character
     public int extractSpecialCharacterWordCount() {
         int count = 0;
         for(int i = 0; i < words.length; i++) {
@@ -103,92 +88,17 @@ public class EmailFeatures {
         return count;
     }
 
-    public int[][] extractWordSizeDistribution(int largest) {
-        int[][] wordSizeDistribution = new int[largest][2];
-        for(int i = 0; i < largest; i++) {
-            wordSizeDistribution[i][0] = i + 1;
-            wordSizeDistribution[i][1] = 0;
-        }
-        for(int i = 0; i < words.length; i++) {
-            wordSizeDistribution[words[i].length()-1][1]++;
-        }
-        return wordSizeDistribution;
-    }
-
-    public double extractMeanWordSize() {
-        int totalLength = 0;
-
-        for(String word : words) {
-            totalLength += word.length();
-        }
-        if(totalLength == 0) {
-            return 0.0;
-        }
-        return (double)totalLength / words.length;
-    }
-
-    public double extractMedianWordSize() {
-        int[] wordLengths = new int[words.length];
-        for(int i = 0; i < words.length; i++) {
-            wordLengths[i] = words[i].length();
-        }
-        Arrays.sort(wordLengths);
-
-        int middle = wordLengths.length / 2;
-        if(wordLengths.length % 2 == 0) {
-            return((wordLengths[middle - 1] + wordLengths[middle]) / 2.0);
-        }
-        else {
-            return wordLengths[middle];
-        }
-    }
-
-    public int extractModeWordSize() {
-        HashMap<Integer, Integer> lengthFrequency = new HashMap<>();
-
-        for(String word : words) {
-            int length = word.length();
-            lengthFrequency.put(length, lengthFrequency.getOrDefault(length, 0) + 1);
-        }
-        int modeLength = 0;
-        int maxFrequency = 0;
-
-        for(Map.Entry<Integer, Integer> entry : lengthFrequency.entrySet()) {
-            if(entry.getValue() > maxFrequency) {
-                maxFrequency = entry.getValue();
-                modeLength = entry.getKey();
-            }
-        }
-        return modeLength;
-    }
-
-
-
-    public int getWordCount() {
-        return wordCount;
-    }
+    // Getters for each individual email feature
     public int getUrlCount() {
         return urlCount;
     }
     public int getSpecialCharacterWordCount() {
         return specialCharacterWordCount;
     }
-    public String[][] getWordFrequency() {
+    public HashMap<String, Integer> getWordFrequency() {
         return wordFrequency;
     }
     public String[][] getNGramFrequency() {
         return nGramFrequency;
-    }
-    public int[][] getWordSizeDistribution() {
-        return wordSizeDistribution;
-    }
-    public double getMeanWordSize() {
-        return meanWordSize;
-    }
-    public double getMedianWordSize() {
-        return medianWordSize;
-    }
-    public int getModeWordSize() {
-        return modeWordSize;
     }
 }
